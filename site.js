8e65f222193caef1922fafe9b7e3e8904df4a63b
Dylan Scelza <dylan@Dylans-MacBook-Pro.local>
2016-08-30 01:43:37 -0400

    JavaScript functions for contact buttons

--- site.js
+++ site.js
@@ -1,3 +1,17 @@
 <!--
 
+    //Linked In contact btn click
+    function linkedin_btn_click(){
+        window.location = "https://www.linkedin.com/in/dylanscelza";
+    }
+
+    //Email contact btn click. Anti-spam protection.
+    function email_btn_click(){
+        var firstPhrase = "dsc";
+        var secondPhrase = "elza";
+        var thirdPhrase = "@umich.edu";
+        
+        document.getElementById("emailContent").innerHTML = "Email: " + firstPhrase + secondPhrase + thirdPhrase;
+        console.log(document.getElementById("emailContent").innerHTML);
+    }
 -->
