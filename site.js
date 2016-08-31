09a6c1fbb8a8ccb1f162b4db76d2ffe05b6f349d
Dylan Scelza <dylan@Dylans-MacBook-Pro.local>
2016-08-31 02:00:56 -0400

    Auto collapse navbar items once item is clicked

--- site.js
+++ site.js
@@ -36,28 +36,28 @@
         });
     });
 
-    //
+    //Collapse toggle button when nav item clicked
+    function minimizeNavbar(){
+        if ($("#nav-toggle-btn").is(":visible"))
+            $("#nav-toggle-btn").click();
+    }
+
     // Add smooth scrolling to all links inside a navbar
-$("#navbar-main a").on('click', function(event){
-
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash (#)
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){
-
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    }) // End if statement
-  };
-});
+    $("#navbar-main a").on('click', function(event){
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        // Store hash (#)
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+        $('html, body').animate({
+        scrollTop: $(hash).offset().top
+        }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+        }) // End if statement
+    };
+    });
 -->
