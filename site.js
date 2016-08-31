3e39e57a760a018ccc99dd5e4fd9948d1ca9f786
Dylan Scelza <dylan@Dylans-MacBook-Pro.local>
2016-08-31 01:44:38 -0400

    Added navbar with scrollspy and animated scroll

--- site.js
+++ site.js
@@ -24,4 +24,40 @@
             modalImg.style = "width: 80%; width: 80vw; height:auto;"
         $('#screenshotModal').modal('toggle');
     }
+
+    //Fades in navbar on-scroll
+    $(document).ready(function(){
+        //hide navbar onpage load
+        $("#navbar-main").hide();
+        $(window).scroll(function () {
+        // set distance to scroll before fadeIn navbar
+        if ($(this).scrollTop() > 25) 
+            $('#navbar-main').fadeIn();
+        });
+    });
+
+    //
+    // Add smooth scrolling to all links inside a navbar
+$("#navbar-main a").on('click', function(event){
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash (#)
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    }) // End if statement
+  };
+});
 -->
